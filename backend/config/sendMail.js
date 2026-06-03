fa0c35d679e930cec41f65bc835bf5388d216487
abhishek <abhishektm444@gmail.com>
2026-06-03 19:47:54 +0530

    fix resend email

--- backend/config/sendMail.js
+++ backend/config/sendMail.js
@@ -1,27 +1,26 @@
 const { Resend } = require("resend");
 
+if (!process.env.RESEND_API_KEY) {
+  console.log("❌ RESEND_API_KEY missing");
+}
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendOTP = async (email, otp) => {
   try {
 
-    if (!process.env.RESEND_API_KEY) {
-      console.log("❌ RESEND_API_KEY NOT FOUND");
-      return false;
-    }
-
-    const data = await resend.emails.send({
+    const response = await resend.emails.send({
       from: "Job Portal <onboarding@resend.dev>",
       to: email,
-      subject: "Your OTP Code",
-      html: `<h3>Your OTP is: ${otp}</h3>`
+      subject: "OTP Verification",
+      html: `<h2>Your OTP is: ${otp}</h2>`
     });
 
-    console.log("✅ OTP email sent:", data);
+    console.log("✅ OTP email sent:", response);
     return true;
 
   } catch (err) {
-    console.log("❌ EMAIL FAILED FULL ERROR:", err);
+    console.log("❌ EMAIL FAILED:", err);
     return false;
   }
 };
